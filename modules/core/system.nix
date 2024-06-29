2650219cb1ee973c75769fe0df508de62d1d8723
Kyle <56144092+txkyel@users.noreply.github.com>
2024-06-29 11:09:19 -0400

    Use cache for hyprland

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -2,9 +2,11 @@
 {
   nix = {
     settings = {
+      auto-optimise-store = true;
       experimental-features = [ "nix-command" "flakes" ];
+      substituters = ["https://hyprland.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
     };
-    optimise.automatic = true;
     gc = {
       automatic = true;
       dates = "weekly";
