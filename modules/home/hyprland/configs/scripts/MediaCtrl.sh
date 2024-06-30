d0626846b71ade84223a5422db7a6637c93ede84
Kyle Xiao <kylexiao20@gmail.com>
2024-06-30 19:29:24 -0400

    Add JaKooLit/Hyprland-Dots to nix config

---
+++ modules/home/hyprland/configs/scripts/MediaCtrl.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
+# Playerctl
+
+music_icon="$HOME/.config/swaync/icons/music.png"
+
+# Play the next track
+play_next() {
+    playerctl next
+}
+
+# Play the previous track
+play_previous() {
+    playerctl previous
+}
+
+# Toggle play/pause
+toggle_play_pause() {
+    playerctl play-pause
+}
+
+# Stop playback
+stop_playback() {
+    playerctl stop
+}
+
+# Get media control action from command line argument
+case "$1" in
+    "--nxt")
+        play_next
+        ;;
+    "--prv")
+        play_previous
+        ;;
+    "--pause")
+        toggle_play_pause
+        ;;
+    "--stop")
+        stop_playback
+        ;;
+    *)
+        echo "Usage: $0 [--nxt|--prv|--pause|--stop]"
+        exit 1
+        ;;
+esac
