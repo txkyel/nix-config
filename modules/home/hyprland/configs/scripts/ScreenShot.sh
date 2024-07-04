98686571f9a5b77695d2a79ce9de9f75192afce5
Kyle Xiao <kylexiao20@gmail.com>
2024-07-04 11:33:11 -0400

    Add var for common notify args in screenshot.sh

--- modules/home/hyprland/configs/scripts/ScreenShot.sh
+++ modules/home/hyprland/configs/scripts/ScreenShot.sh
@@ -9,13 +9,14 @@ time=$(date "+%Y-%m-%d_%H%M%S")
 file="${dir}/Screenshot_${time}_${RANDOM}.png"
 active_window_class=$(hyprctl -j activewindow | jq -r '(.class)')
 active_window_file="${dir}/Screenshot_${time}_${active_window_class}.png"
+notify_cmd_base="notify-send -a "Screenshot" -u low"
 
 notify_screenshot() {
     # Exit early if image failed to save
-    [ ! -f "$1" ] && notify-send "Screenshot captured but did not save" && exit 1
+    [ ! -f "$1" ] && $notify_cmd_base "Screenshot captured but did not save" && exit 1
 
     # If default action is taken for notification, execute swappy with image
-    ret=$(notify-send -u low -t 1 -i $1 --action="default=Mark up image" "$2" "Click to markup image")
+    ret=$($notify_cmd_base -t 1 -i "$1" --action="default=Mark up image" "$2" "Click to markup image")
     [ "default" == "$ret" ] && swappy -f "$1"
 }
 
