19ead8d02083b099011d707ddf56112c07eae6f8
Kyle Xiao <kylexiao20@gmail.com>
2024-07-10 19:33:57 -0400

    Remove workspace "prev" switching behaviour

--- modules/home/hyprland/configs/scripts/Workspace.sh
+++ modules/home/hyprland/configs/scripts/Workspace.sh
@@ -3,30 +3,21 @@
 # Qtile like workspace changing behaviour
 # Based on https://github.com/Jareng/dotfiles/blob/7cf34f020e18f8b5bb5a977baac1f1412a177d7d/.config/hypr/scripts/workspace_qtile.sh
 
-monitors="$(hyprctl monitors -j)"
+targetworkspace="$1"
 
 # Workspace must be selected for this command to work
-if [ "$1" = "" ]; then
+if [ "$targetworkspace" = "" ]; then
     echo -e "Command format:\n\tWorkspace.sh <workspace number>"
     exit 1
 fi
 
-targetworkspace="$1"
-
-prevworkspace_temp="/tmp/hypr/prevworkspace_temp"
-prevworkspace="$(cat "$prevworkspace_temp")"
+monitors="$(hyprctl monitors -j)"
 currworkspace="$(echo "$monitors" | jq '.[] | select(.focused==true) | .activeWorkspace.id')"
 
 # Save current workspace before switching to new
 # Exit if current workspace is target workspace
-if [ "$targetworkspace" != "$currworkspace" ]; then
-	echo "$currworkspace" >"$prevworkspace_temp"
-fi
-
-# Switch to previous workspace and quit
-if [ "$targetworkspace" == "prev" ]; then
-	hyprctl dispatch workspace "$prevworkspace"
-	exit 0
+if [ "$targetworkspace" == "$currworkspace" ]; then
+    exit 0
 fi
 
 activemonitor="$(echo "$monitors" | jq '.[] | select(.focused==true) | .id')"
