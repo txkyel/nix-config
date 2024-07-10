75ab4a5938c5e42882b2a4c8f2a814a0b81aa98b
Kyle Xiao <kylexiao20@gmail.com>
2024-07-09 21:17:20 -0400

    Add move active window and follow keybinds

--- modules/home/hyprland/configs/scripts/Workspace.sh
+++ modules/home/hyprland/configs/scripts/Workspace.sh
@@ -1,21 +1,30 @@
 #!/bin/sh
 
-# Qtile based workspace changing behaviour
+# Qtile like workspace changing behaviour
 # Based on https://github.com/Jareng/dotfiles/blob/7cf34f020e18f8b5bb5a977baac1f1412a177d7d/.config/hypr/scripts/workspace_qtile.sh
 
 monitors="$(hyprctl monitors -j)"
+
+# Workspace must be selected for this command to work
+if [ "$1" = "" ]; then
+    echo -e "Command format:\n\tWorkspace.sh <workspace number>"
+    exit 1
+fi
+
+targetworkspace="$1"
 
 prevworkspace_temp="/tmp/hypr/prevworkspace_temp"
 prevworkspace="$(cat "$prevworkspace_temp")"
 currworkspace="$(echo "$monitors" | jq '.[] | select(.focused==true) | .activeWorkspace.id')"
 
 # Save current workspace before switching to new
-if [ "$1" != "$currworkspace" ]; then
+# Exit if current workspace is target workspace
+if [ "$targetworkspace" != "$currworkspace" ]; then
 	echo "$currworkspace" >"$prevworkspace_temp"
 fi
 
 # Switch to previous workspace and quit
-if [[ "$1" == "prev" ]]; then
+if [ "$targetworkspace" == "prev" ]; then
 	hyprctl dispatch workspace "$prevworkspace"
 	exit 0
 fi
@@ -24,19 +33,16 @@ activemonitor="$(echo "$monitors" | jq '.[] | select(.focused==true) | .id')"
 passivemonitor="$(echo "$monitors" | jq '.[] | select(.focused==false) | .id')"
 passiveworkspace="$(echo "$monitors" | jq '.[] | select(.focused==false) | .activeWorkspace.id')"
 
-# Swap
-if [ "$1" = "" ]; then
-	workspace="$(echo "$monitors" | jq '.[] | select(.focused==false) | .activeWorkspace.id')"
-	exit 0
-else
-	workspace="$1"
+if [ $2 = "switch" ]; then
+    hyprctl dispatch movetoworkspacesilent "$targetworkspace"
 fi
 
-if [ "$workspace" = "$passiveworkspace" ] && [ "$activemonitor" != "$passivemonitor" ]; then
+
+if [ "$targetworkspace" = "$passiveworkspace" ] && [ "$activemonitor" != "$passivemonitor" ]; then
 	hyprctl dispatch swapactiveworkspaces "$activemonitor" "$passivemonitor"
 else
-	hyprctl dispatch moveworkspacetomonitor "$workspace" "$activemonitor"
-	hyprctl dispatch workspace "$workspace"
+	hyprctl dispatch moveworkspacetomonitor "$targetworkspace" "$activemonitor"
+	hyprctl dispatch workspace "$targetworkspace"
 fi
 
 exit 0
