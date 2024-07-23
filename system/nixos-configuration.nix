bfc12f36ddb1ddb4483b138933fb9c5ed7d46d05
Kyle Xiao <kylexiao20@gmail.com>
2024-07-23 17:35:49 -0400

    Turn on auto upgrade

--- system/nixos-configuration.nix
+++ system/nixos-configuration.nix
@@ -17,16 +17,15 @@
         options = "--delete-older-than 7d";
         persistent = true;
     };
-    # Turn off auto upgrade until hyprland is fixed https://github.com/hyprwm/Hyprland/issues/6973
-    #system.autoUpgrade = {
-    #    enable = true;
-    #    flake = "github:txkyel/nix-config";
-    #    flags = [
-    #        "--show-trace"
-    #        "-L"
-    #    ];
-    #    persistent = true;
-    #};
+    system.autoUpgrade = {
+        enable = true;
+        flake = "github:txkyel/nix-config";
+        flags = [
+            "--show-trace"
+            "-L"
+        ];
+        persistent = true;
+    };
 
     # This should match the version you used to install nixos
     system.stateVersion = "24.05";
