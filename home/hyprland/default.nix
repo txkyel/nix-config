7aae7acd23760e86513ea0f5f0f10c7fb0b28511
Kyle Xiao <kylexiao20@gmail.com>
2024-07-24 19:28:01 -0400

    Move hyprland dep packages to home-manager

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -1,9 +1,41 @@
-{ config, username, ... }:
+{ inputs, pkgs, config, username, host, ... }:
 let
     # TODO: figure out how to unhardcode this, and make it dynamically determine where
     # the project source is located
     configPath = /home/${username}/nix-config/home/hyprland/hypr;
+    hyprland =
+    if (host == "x230") then
+        inputs.hyprland.packages.${pkgs.system}.hyprland.override { legacyRenderer = true; }
+    else
+        inputs.hyprland.packages.${pkgs.system}.hprland;
+    xdph = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
 in
 {
+    # Link config files to project files
     xdg.configFile.hypr.source = config.lib.file.mkOutOfStoreSymlink configPath;
+
+    # Package
+    home.packages = with pkgs; [
+        # Hyprland utils
+        hyprcursor
+        hypridle
+        hyprlock
+        pyprland
+
+        # Theming
+        qt5ct
+        qt6ct
+        qt6.qtwayland
+        libsForQt5.qtstyleplugin-kvantum # kvantum
+        qt6Packages.qtstyleplugin-kvantum # kvantum
+        nwg-look
+        swww
+        yad
+
+        # Additional window manager utils
+        wlogout
+        yad # Used for keybind hints
+    ];
+
+    programs.hyprlock.enable = true;
 }
--- system/hyprland.nix
+++ system/hyprland.nix
@@ -8,7 +8,6 @@ let
     xdph = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
 in
 {
-    # TODO: Move back to home-manager module
     environment.sessionVariables.NIXOS_OZONE_WL = "1";
     programs.hyprland = {
         enable = true;
@@ -27,32 +26,11 @@ in
 
     # Hyprland packages
     environment.systemPackages = with pkgs; [
-        # Hyprland utils
-        hyprcursor
-        hypridle
-        hyprlock
-        pyprland
-
-        # Appearance
-        qt5ct
-        qt6ct
-        qt6.qtwayland
-        libsForQt5.qtstyleplugin-kvantum # kvantum
-        qt6Packages.qtstyleplugin-kvantum # kvantum
-        nwg-look
-        swww
-
         # Additional window manager utils
-        wlogout
         xdg-user-dirs
         xdg-utils
-        yad
 
         # Terminal
         kitty
     ];
-
-    programs = {
-        hyprlock.enable = true;
-    };
 }
