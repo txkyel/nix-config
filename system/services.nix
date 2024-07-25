7e887fbe5fa5f984c5d3e53630e8af62ac71fd13
Kyle Xiao <kylexiao20@gmail.com>
2024-07-24 21:20:41 -0400

    Add back envfs service for shebang use

--- system/services.nix
+++ system/services.nix
@@ -3,6 +3,7 @@
     programs.dconf.enable = true;
     services = {
         dbus.enable = true;
+        envfs.enable = true; # For shebang execution
         fstrim.enable = true;
         gvfs.enable = true;
     };
