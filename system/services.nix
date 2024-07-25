0286baddad6a2c323682b5391f4afd812e79fcd4
Kyle Xiao <kylexiao20@gmail.com>
2024-07-24 20:00:15 -0400

    Remove unused services

--- system/services.nix
+++ system/services.nix
@@ -3,10 +3,7 @@
     programs.dconf.enable = true;
     services = {
         dbus.enable = true;
-        envfs.enable = true;
         fstrim.enable = true;
-        fwupd.enable = true;
         gvfs.enable = true;
-        udev.enable = true;
     };
 }
