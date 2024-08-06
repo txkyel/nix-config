3600af1d6844d5497f493093b875beb45a44ee06
Kyle Xiao <kylexiao20@gmail.com>
2024-08-06 18:28:14 -0400

    Fix gamescope mangoapp config

--- system/games.nix
+++ system/games.nix
@@ -1,10 +1,20 @@
+{ config, username, ... }:
+let
+    home = config.users.users.${username}.home;
+    MangoHudConf = "${home}/.config/MangoHud/MangoHud.conf";
+in
 {
     programs.steam = {
         enable = true;
-        gamescopeSession.enable = true;
         remotePlay.openFirewall = true;
         dedicatedServer.openFirewall = true;
         localNetworkGameTransfers.openFirewall = true;
     };
     programs.gamemode.enable = true;
+    programs.gamescope = {
+        enable = true;
+        env = {
+            MANGOHUD_CONFIGFILE = MangoHudConf;
+        };
+    };
 }
