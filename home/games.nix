05d6c823639698a47e9932839787269c2f60a31b
Kyle Xiao <kylexiao20@gmail.com>
2024-08-05 22:08:37 -0400

    Add steam_dev.cfg to home-manager

--- home/games.nix
+++ home/games.nix
@@ -11,4 +11,12 @@
             cpu_power = true;
         };
     };
+
+    # Write config file to enable faster downloads and shader compilation
+    xdg.dataFile."Steam/steam_dev.cfg".text = ''
+        @nClientDownloadEnableHTTP2PlatformLinux 0
+        @fDownloadRateImprovementToAddAnotherConnection 1.0
+        @cMaxInitialDownloadSources 25
+        unShaderBackgroundProcessingThreads 8
+    '';
 }
