100027264846b3dba84dac8cd2aa320a645b8c05
Kyle Xiao <kylexiao20@gmail.com>
2024-09-23 00:30:29 -0400

    WIP mpv config

--- home/mpv.nix
+++ home/mpv.nix
@@ -5,8 +5,20 @@
         bindings = {
             MBTN_LEFT = "cycle pause";
         };
-        scripts = with pkgs; [
-            mpvScripts.mpris
+        scripts = with pkgs.mpvScripts; [
+            modernx-zydezu
+            mpris
+            thumbfast
         ];
     };
+
+    # ModernX config
+    xdg.configFile."mpv/script-opts/modernx.conf".text = ''
+        compactmode=no
+        showontop=no
+    '';
+
+    # TODO: Enable single instance.
+    # Like this with desktop entry and as default app
+    # https://www.reddit.com/r/pop_os/comments/ox6t7n/run_mpv_in_single_instance/i1ojqw4/
 }
