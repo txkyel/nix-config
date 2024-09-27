6e728200501b1f33d14d62976cddb7c92974d709
Kyle Xiao <kylexiao20@gmail.com>
2024-09-27 00:12:55 -0400

    Update boot settings

--- system/bootloader.nix
+++ system/bootloader.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }:
 {
-    boot.loader.efi.canTouchEfiVariables = true;
-    boot.loader.timeout = 2;
     boot.kernelPackages = pkgs.linuxPackages_latest;
+    boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.timeout = 1;
 
     boot.loader.grub.enable = true;
     boot.loader.grub.devices = [ "nodev" ]; # Allow Windows boot entry
@@ -14,4 +14,16 @@
             fwsetup
         }
     '';
+
+    # Silent Boot
+    boot.kernelParams = [
+        "quiet"
+        "splash"
+        "vga=current"
+        "rd.systemd.show_status=false"
+        "rd.udev.log_level=3"
+        "udev.log_priority=3"
+    ];
+    boot.consoleLogLevel = 0;
+    boot.initrd.verbose = false;
 }
