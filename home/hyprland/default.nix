0dd099dac4bfd7042c9b01959cb03fa20445b17c
Kyle Xiao <kylexiao20@gmail.com>
2024-10-21 13:18:05 -0400

    Revert cliphist fix

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -24,15 +24,7 @@ in
         grim
         slurp
         swappy
-        (cliphist.overrideAttrs (_old: {
-          src = pkgs.fetchFromGitHub {
-            owner = "sentriz";
-            repo = "cliphist";
-            rev = "c49dcd26168f704324d90d23b9381f39c30572bd";
-            sha256 = "sha256-2mn55DeF8Yxq5jwQAjAcvZAwAg+pZ4BkEitP6S2N0HY=";
-          };
-          vendorHash = "sha256-M5n7/QWQ5POWE4hSCMa0+GOVhEDCOILYqkSYIGoy/l0=";
-        }))
+        cliphist
         wl-clipboard
         libnotify
         swaynotificationcenter
