21519ed29010bb37ce95c3ee0d6f9eb112656c30
Kyle Xiao <kylexiao20@gmail.com>
2024-12-03 13:50:25 -0500

    Enable bluetooth on boot

--- system/bluetooth.nix
+++ system/bluetooth.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 {
     hardware.bluetooth.enable = true;
-    #hardware.bluetooth.powerOnBoot = true;
+    hardware.bluetooth.powerOnBoot = true;
     services.blueman.enable = true;
 }
