d10d951d445fe7e229bea2a8008590cdfbacc21a
Kyle Xiao <kylexiao20@gmail.com>
2025-01-06 18:26:20 -0500

    Add VR support via wivrn

--- home/games.nix
+++ home/games.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
     home.packages = with pkgs; [
         mangohud
@@ -22,5 +22,27 @@
         @fDownloadRateImprovementToAddAnotherConnection 1.0
         @cMaxInitialDownloadSources 25
         unShaderBackgroundProcessingThreads 8
+    '';
+
+    # VR
+    xdg.configFile."openxr/1/active_runtime.json".source = "${pkgs.wivrn}/share/openxr/1/openxr_wivrn.json";
+    xdg.configFile."openvr/openvrpaths.vrpath".text = ''
+      {
+        "config" :
+        [
+          "${config.xdg.dataHome}/Steam/config"
+        ],
+        "external_drivers" : null,
+        "jsonid" : "vrpathreg",
+        "log" :
+        [
+          "${config.xdg.dataHome}/Steam/logs"
+        ],
+        "runtime" :
+        [
+          "${pkgs.opencomposite}/lib/opencomposite"
+        ],
+        "version" : 1
+      }
     '';
 }
--- home/git.nix
+++ home/git.nix
@@ -1,6 +1,7 @@
 {
   programs.git = {
     enable = true;
+    lfs.enable = true;
     userEmail = "kylexiao20@gmail.com";
     userName = "Kyle Xiao";
   };
