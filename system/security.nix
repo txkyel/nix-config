8bc935fd0e84677148679fc7b1221a932283b9f2
Kyle Xiao <kylexiao20@gmail.com>
2025-01-16 22:59:34 -0500

    Autostart CoreCtrl

--- system/security.nix
+++ system/security.nix
@@ -2,5 +2,16 @@
     security = {
         pam.services.swaylock.text = "auth include login";
         polkit.enable = true;
+        polkit.extraConfig = ''
+            polkit.addRule(function(action, subject) {
+                if ((action.id == "org.corectrl.helper.init" ||
+                     action.id == "org.corectrl.helperkiller.init") &&
+                    subject.local == true &&
+                    subject.active == true &&
+                    subject.isInGroup("users")) {
+                        return polkit.Result.YES;
+                }
+            });
+        '';
     };
 }
