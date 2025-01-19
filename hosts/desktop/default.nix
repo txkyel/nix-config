d62f5a7f33a3661b034d7b6dfae50986b5e1b984
Kyle Xiao <kylexiao20@gmail.com>
2025-01-18 23:40:19 -0500

    Remove kernel patch for steamvr

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -30,14 +30,4 @@
     boot.blacklistedKernelModules = [ "k10temp" ];
     boot.kernelModules = [ "zenpower" ];
     boot.extraModulePackages = [ config.boot.kernelPackages.zenpower ];
-    boot.kernelPatches = [
-      {
-        name = "amdgpu-ignore-ctx-privileges";
-        patch = pkgs.fetchpatch {
-          name = "cap_sys_nice_begone.patch";
-          url = "https://github.com/Frogging-Family/community-patches/raw/master/linux61-tkg/cap_sys_nice_begone.mypatch";
-          hash = "sha256-Y3a0+x2xvHsfLax/uwycdJf3xLxvVfkfDVqjkxNaYEo=";
-        };
-      }
-    ];
 }
