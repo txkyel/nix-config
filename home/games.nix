8b14589828c6ff2a9663ecea5c96033e599199f4
Kyle Xiao <kylexiao20@gmail.com>
2025-02-09 21:51:08 -0500

    Update stats monitored by mangohud

--- home/games.nix
+++ home/games.nix
@@ -8,11 +8,15 @@
     programs.mangohud = {
         enable = true;
         settings = {
+            gpu_core_clock = true;
+            gpu_mem_clock = true;
             gpu_power = true;
             gpu_temp = true;
+            vram = true;
             cpu_power = true;
             cpu_temp = true;
             table_columns = 5;
+
         };
     };
 
