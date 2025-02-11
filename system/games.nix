6f57853a7dc287e72feadac1b8e7aaed417df57a
Kyle Xiao <kylexiao20@gmail.com>
2025-02-10 22:48:05 -0500

    Switch back to envision

--- system/games.nix
+++ system/games.nix
@@ -33,52 +33,7 @@ in
             ppfeaturemask = "0xffffffff";
         };
     };
-    systemd.user.services.monado.environment = {
-        STEAMVR_LH_ENABLE = "1";
-        XRT_COMPOSITOR_COMPUTE = "1";
-    };
     programs.envision = {
-        enable = false;
-    };
-    services.wivrn = {
-        # Use wivrn in case envision breaks
         enable = true;
-        openFirewall = true;
-        defaultRuntime = true;
-        config.enable = true;
-        config.json = {
-            scale = 0.5;
-            bitrate = 200000000;
-            application = [ pkgs.wlx-overlay-s ];
-            encoders = [
-                {
-                    encoder = "vaapi";
-                    codec = "h265";
-                    width = 0.5;
-                    height = 0.25;
-                    offset_x = 0.0;
-                    offset_y = 0.0;
-                    group = 0;
-                }
-                {
-                    encoder = "vaapi";
-                    codec = "h265";
-                    width = 0.5;
-                    height = 0.75;
-                    offset_x = 0.0;
-                    offset_y = 0.25;
-                    group = 0;
-                }
-                {
-                    encoder = "vaapi";
-                    codec = "h265";
-                    width = 0.5;
-                    height = 1.0;
-                    offset_x = 0.5;
-                    offset_y = 0.0;
-                    group = 0;
-                }
-            ];
-        };
     };
 }
