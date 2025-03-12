9bd2bf545612841e0054a5cbf9b6f943b5dc155e
Kyle Xiao <kylexiao20@gmail.com>
2025-03-11 22:54:58 -0400

    Use smart indent instead of treesitter indent

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -6,17 +6,33 @@ let
       config.vim = {
         viAlias = true;
         vimAlias = true;
-        autocomplete.nvim-cmp.enable = true;
-        statusline.lualine.enable = true;
-        telescope.enable = true;
+
+        options = {
+          autoindent = true;
+          expandtab = true;
+          shiftwidth = 4;
+          smartindent = true;
+          tabstop = 4;
+        };
 
         languages = {
           enableLSP = true;
           enableFormat = true;
-          enableTreesitter = true;
 
           nix.enable = true;
           python.enable = true;
+        };
+
+        autocomplete.nvim-cmp.enable = true;
+        statusline.lualine.enable = true;
+        telescope.enable = true;
+        treesitter = {
+          enable = true;
+          context.enable = true;
+          indent.enable = false;
+        };
+        visuals = {
+          indent-blankline.enable = true;
         };
       };
     }];
