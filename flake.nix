48eeca863f36a3cc597e52a352a0ff3df689d208
Kyle Xiao <kylexiao20@gmail.com>
2025-03-11 20:34:10 -0400

    Remove nixvim

--- flake.nix
+++ flake.nix
@@ -16,10 +16,6 @@
             inputs.nixpkgs.follows = "nixpkgs";
         };
 
-        nixvim = {
-            url = "github:nix-community/nixvim";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
         nvf = {
             url = "github:notashelf/nvf";
             inputs.nixpkgs.follows = "nixpkgs";
