95e6624fb99d469d16dd31034885a554ed1e4158
Kyle Xiao <kylexiao20@gmail.com>
2025-03-11 22:08:51 -0400

    Add vi and vim alias

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -2,28 +2,23 @@
 let
   customNeovim = inputs.nvf.lib.neovimConfiguration {
     inherit pkgs;
-    modules = [
-      {
-        config.vim = {
-          autocomplete.nvim-cmp.enable = true;
-          statusline.lualine.enable = true;
-          telescope.enable = true;
+    modules = [{
+      config.vim = {
+        viAlias = true;
+        vimAlias = true;
+        autocomplete.nvim-cmp.enable = true;
+        statusline.lualine.enable = true;
+        telescope.enable = true;
 
-          languages = {
-            enableLSP = true;
-            enableFormat = true;
-            enableTreesitter = true;
+        languages = {
+          enableLSP = true;
+          enableFormat = true;
+          enableTreesitter = true;
 
-            nix.enable = true;
-            python.enable = true;
-          };
+          nix.enable = true;
+          python.enable = true;
         };
-      }
-    ];
+      };
+    }];
   };
-in
-{
-  home.packages = [
-    customNeovim.neovim
-  ];
-}
+in { home.packages = [ customNeovim.neovim ]; }
