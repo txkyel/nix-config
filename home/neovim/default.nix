e333bd2087def03c3d0a8b23cd03fcae6da71f2a
Kyle Xiao <kylexiao20@gmail.com>
2025-03-14 16:30:24 -0400

    Add format on save

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -1,47 +1,54 @@
-{ inputs, pkgs, ... }:
-let
+{
+  inputs,
+  pkgs,
+  ...
+}: let
   customNeovim = inputs.nvf.lib.neovimConfiguration {
     inherit pkgs;
-    modules = [{
-      config.vim = {
-        viAlias = true;
-        vimAlias = true;
-        useSystemClipboard = true;
+    modules = [
+      {
+        config.vim = {
+          viAlias = true;
+          vimAlias = true;
+          useSystemClipboard = true;
 
-        options = {
-          autoindent = true;
-          expandtab = true;
-          shiftwidth = 4;
-          tabstop = 4;
-        };
+          options = {
+            autoindent = true;
+            expandtab = true;
+            shiftwidth = 4;
+            tabstop = 4;
+          };
 
-        languages = {
-          enableLSP = true;
-          enableFormat = true;
-          enableTreesitter = true;
+          lsp = {
+            formatOnSave = true;
+          };
 
-          nix.enable = true;
-          python = {
+          languages = {
+            enableLSP = true;
+            enableFormat = true;
+            enableTreesitter = true;
+
+            nix.enable = true;
+            python = {
+              enable = true;
+              lsp.server = "pyright";
+            };
+          };
+
+          autocomplete.nvim-cmp.enable = true;
+          autopairs.nvim-autopairs.enable = true;
+          statusline.lualine.enable = true;
+          telescope.enable = true;
+          treesitter = {
             enable = true;
-            lsp.server = "pyright";
+            context.enable = true;
+            indent.enable = true;
+          };
+          visuals = {
+            indent-blankline.enable = true;
           };
         };
-
-        autocomplete.nvim-cmp.enable = true;
-        autopairs.nvim-autopairs.enable = true;
-        statusline.lualine.enable = true;
-        telescope.enable = true;
-        treesitter = {
-          enable = true;
-          context.enable = true;
-          indent.enable = true;
-          # TODO: Fix jank nix indentation
-          indent.disable = ["nix"];
-        };
-        visuals = {
-          indent-blankline.enable = true;
-        };
-      };
-    }];
+      }
+    ];
   };
-in { home.packages = [ customNeovim.neovim ]; }
+in {home.packages = [customNeovim.neovim];}
