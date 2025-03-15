71b6ed247a93b2b409dc69005e59d08d4e999ca4
Kyle Xiao <kylexiao20@gmail.com>
2025-03-15 13:04:51 -0400

    Migrate volume script to packages

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -2,12 +2,14 @@
 let
   airplane-mode = pkgs.writeScriptBin "airplane-mode" (builtins.readFile ./airplane-mode.sh);
   screenshot = pkgs.writeScriptBin "screenshot" (builtins.readFile ./screenshot.sh);
+  volume = pkgs.writeScriptBin "volume" (builtins.readFile ./volume.sh);
   workspace-qtile = pkgs.writeScriptBin "workspace-qtile" (builtins.readFile ./workspace-qtile.sh);
 in
 {
   home.packages = [
     airplane-mode
     screenshot
+    volume
     workspace-qtile
   ];
 }
