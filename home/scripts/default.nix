7acd7bb9dc41fd2b368683e3ba1e9de515e0b30b
Kyle Xiao <kylexiao20@gmail.com>
2025-03-15 17:42:15 -0400

    Migrate ClipManager to packages

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 let
   airplane-mode = pkgs.writeScriptBin "airplane-mode" (builtins.readFile ./airplane-mode.sh);
+  clip-menu = pkgs.writeScriptBin "clip-menu" (builtins.readFile ./clip-menu.sh);
   screenshot = pkgs.writeScriptBin "screenshot" (builtins.readFile ./screenshot.sh);
   volume = pkgs.writeScriptBin "volume" (builtins.readFile ./volume.sh);
   wallpaper = pkgs.writeScriptBin "wallpaper" (builtins.readFile ./wallpaper.sh);
@@ -9,6 +10,7 @@ in
 {
   home.packages = [
     airplane-mode
+    clip-menu
     screenshot
     volume
     wallpaper
