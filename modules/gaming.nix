f9d404300b460d0e7b14c01440fcc96d14808a37
Kyle Xiao <kylexiao20@gmail.com>
2025-03-15 09:49:55 -0400

    Improve gaming module format

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -6,7 +6,8 @@
   ...
 }:
 let
-  inherit (lib) mkIf mkEnableOption optionals;
+  inherit (lib.options) mkEnableOption;
+  inherit (lib.modules) mkIf mkMerge;
   cfg = config.modules.gaming;
 
   MangoHudConf = "${config.users.users.${username}.home}/.config/MangoHud/MangoHud.conf";
@@ -17,77 +18,81 @@ in
     enableVR = mkEnableOption "Enabling VR";
   };
 
-  config = mkIf cfg.enable {
-    programs.steam = {
-      enable = true;
-      remotePlay.openFirewall = true;
-      dedicatedServer.openFirewall = true;
-      localNetworkGameTransfers.openFirewall = true;
-    };
-    programs.gamemode.enable = true;
-    programs.gamescope = {
-      enable = true;
-      env = {
-        MANGOHUD_CONFIGFILE = MangoHudConf;
-      };
-    };
-    programs.corectrl = {
-      enable = true;
-      gpuOverclock = {
+  config = mkIf cfg.enable (mkMerge [
+    {
+      programs.steam = {
         enable = true;
-        ppfeaturemask = "0xffffffff";
+        remotePlay.openFirewall = true;
+        dedicatedServer.openFirewall = true;
+        localNetworkGameTransfers.openFirewall = true;
       };
-    };
+      programs.gamemode.enable = true;
+      programs.gamescope = {
+        enable = true;
+        env = {
+          MANGOHUD_CONFIGFILE = MangoHudConf;
+        };
+      };
+      programs.corectrl = {
+        enable = true;
+        gpuOverclock = {
+          enable = true;
+          ppfeaturemask = "0xffffffff";
+        };
+      };
 
-    environment.systemPackages =
-      with pkgs;
-      [
+      environment.systemPackages = with pkgs; [
         protonup-qt
         steamtinkerlaunch # Additional mods and games
         owmods-gui # ??
-      ]
-      ++ optionals (cfg.enableVR) [
+      ];
+
+      users.users.${username}.extraGroups = [
+        "gamemode"
+      ];
+
+      # TODO: Figure out how to separate home manager stuff more elegantly
+      home-manager.users.${username} = {
+        home.packages = with pkgs; [
+          r2modman
+        ];
+
+        programs.mangohud.enable = true;
+        programs.mangohud.settings = {
+          gpu_core_clock = true;
+          gpu_mem_clock = true;
+          gpu_power = true;
+          gpu_temp = true;
+          vram = true;
+          cpu_power = true;
+          cpu_temp = true;
+          table_columns = 5;
+        };
+
+        xdg.dataFile."Steam/steam_dev.cfg".text = ''
+          @nClientDownloadEnableHTTP2PlatformLinux 0
+          @fDownloadRateImprovementToAddAnotherConnection 1.0
+          @cMaxInitialDownloadSources 25
+          unShaderBackgroundProcessingThreads 8
+        '';
+      };
+    }
+
+    (mkIf cfg.enableVR {
+      # VR
+      programs.adb.enable = mkIf cfg.enableVR true;
+      programs.envision = mkIf cfg.enableVR {
+        enable = true;
+      };
+
+      environment.systemPackages = with pkgs; [
         wlx-overlay-s # VR overlay
         sidequest
       ];
 
-    users.users.${username}.extraGroups =
-      [
-        "gamemode"
-      ]
-      ++ optionals (cfg.enableVR) [
+      users.users.${username}.extraGroups = [
         "adbusers"
       ];
-
-    home-manager.users.${username} = {
-      home.packages = with pkgs; [
-        r2modman
-      ];
-
-      programs.mangohud.enable = true;
-      programs.mangohud.settings = {
-        gpu_core_clock = true;
-        gpu_mem_clock = true;
-        gpu_power = true;
-        gpu_temp = true;
-        vram = true;
-        cpu_power = true;
-        cpu_temp = true;
-        table_columns = 5;
-      };
-
-      xdg.dataFile."Steam/steam_dev.cfg".text = ''
-        @nClientDownloadEnableHTTP2PlatformLinux 0
-        @fDownloadRateImprovementToAddAnotherConnection 1.0
-        @cMaxInitialDownloadSources 25
-        unShaderBackgroundProcessingThreads 8
-      '';
-    };
-
-    # VR
-    programs.adb.enable = mkIf cfg.enableVR true;
-    programs.envision = mkIf cfg.enableVR {
-      enable = true;
-    };
-  };
+    })
+  ]);
 }
