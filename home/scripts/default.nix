63c6bdafdc5a6ad835c9394e238e90902c16f979
Kyle Xiao <kylexiao20@gmail.com>
2025-03-15 12:58:14 -0400

    Migrate airplane mode script to packages

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -1,10 +1,12 @@
 { pkgs, ... }:
 let
+  airplane-mode = pkgs.writeScriptBin "airplane-mode" (builtins.readFile ./airplane-mode.sh);
   screenshot = pkgs.writeScriptBin "screenshot" (builtins.readFile ./screenshot.sh);
   workspace-qtile = pkgs.writeScriptBin "workspace-qtile" (builtins.readFile ./workspace-qtile.sh);
 in
 {
-  home.packages = with pkgs; [
+  home.packages = [
+    airplane-mode
     screenshot
     workspace-qtile
   ];
