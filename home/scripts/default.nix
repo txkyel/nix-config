001232e738d6dc0c1a662d8130309a1137c49591
Kyle Xiao <kylexiao20@gmail.com>
2025-03-15 12:40:04 -0400

    Migrate workspaces script to packages

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -1,9 +1,11 @@
 { pkgs, ... }:
 let
   screenshot = pkgs.writeScriptBin "screenshot" (builtins.readFile ./screenshot.sh);
+  workspace-qtile = pkgs.writeScriptBin "workspace-qtile" (builtins.readFile ./workspace-qtile.sh);
 in
 {
   home.packages = with pkgs; [
     screenshot
+    workspace-qtile
   ];
 }
