f05be0839cca7707cea81722634b770b02d5a854
Kyle Xiao <kylexiao20@gmail.com>
2025-03-14 20:01:12 -0400

    Add hypridle config

---
+++ home/hyprland/hypridle.nix
@@ -0,0 +1,39 @@
+{
+  config,
+  lib,
+  pkgs,
+}: let
+  lock = "${pkgs.systemd}/bin/loginctl lock-session";
+  brightnessctl = lib.getExe pkgs.brightnessctl;
+in {
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        lock_cmd = lib.getExe config.programs.hyprlock.package;
+        before_sleep_cmd = lock;
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+      };
+      listener = [
+        {
+          timeout = 150;
+          on-timeout = "${brightnessctl} -s set 10";
+          on-resume = "${brightnessctl} -r";
+        }
+        {
+          timeout = 300;
+          on-timeout = lock;
+        }
+        {
+          timeout = 330;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
+        }
+        {
+          timeout = 600;
+          on-timeout = "systemctl suspend";
+        }
+      ];
+    };
+  };
+}
