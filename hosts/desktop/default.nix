e10fef55bfff46486a16a3bd94c131046a6f219f
Kyle Xiao <kylexiao20@gmail.com>
2025-03-29 11:18:13 -0400

    host/desktop: use nixos-hardware

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -1,33 +1,19 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-{ inputs, config, ... }:
+{ inputs, ... }:
 {
   imports = [
     inputs.nixos-hardware.nixosModules.gigabyte-b550
+    inputs.nixos-hardware.nixosModules.common-cpu-amd-zenpower
+    inputs.nixos-hardware.nixosModules.common-gpu-amd
     ./hardware-configuration.nix
-    #./rgb.nix
   ];
 
   system.stateVersion = "24.05";
 
   hardware.logitech.wireless.enable = true;
   hardware.logitech.wireless.enableGraphical = true;
-
-  # Graphics
-  services.xserver.videoDrivers = [ "amdgpu" ];
-  hardware = {
-    enableRedistributableFirmware = true;
-    graphics = {
-      enable = true;
-      enable32Bit = true;
-    };
-  };
-
-  # Enable zenpower sensors
-  boot.blacklistedKernelModules = [ "k10temp" ];
-  boot.kernelModules = [ "zenpower" ];
-  boot.extraModulePackages = [ config.boot.kernelPackages.zenpower ];
 
   modules.gaming.enable = true;
   modules.gaming.enableVR = true;
