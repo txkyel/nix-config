441d145e3292ea569a578c25bc1d287cee6d526f
Kyle Xiao <kylexiao20@gmail.com>
2025-03-28 22:01:04 -0400

    bootloader: switch back to systemd-boot

--- system/bootloader.nix
+++ system/bootloader.nix
@@ -1,29 +1,26 @@
 { pkgs, ... }:
 {
   boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.timeout = 1;
 
-  boot.loader.grub.enable = true;
-  boot.loader.grub.devices = [ "nodev" ]; # Allow Windows boot entry
-  boot.loader.grub.useOSProber = true; # Allow Windows boot entry
-  boot.loader.grub.efiSupport = true; # Allow Windows boot entry
-  boot.loader.grub.configurationLimit = 10;
-  boot.loader.grub.extraEntries = ''
-    menuentry "UEFI Firmware Settings" {
-        fwsetup
-    }
-  '';
+  boot = {
+    loader = {
+      efi.canTouchEfiVariables = true;
+      timeout = 1;
+      systemd-boot.enable = true;
+      systemd-boot.configurationLimit = 10;
+    };
 
-  # Silent Boot
-  boot.kernelParams = [
-    "quiet"
-    "splash"
-    "vga=current"
-    "rd.systemd.show_status=false"
-    "rd.udev.log_level=3"
-    "udev.log_priority=3"
-  ];
-  boot.consoleLogLevel = 0;
-  boot.initrd.verbose = false;
+    # Silent Boot
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "vga=current"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+      "boot.shell_on_fail"
+    ];
+  };
 }
