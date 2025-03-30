af2fe62ca1fd01bd0b4379718a6a022672711a9c
Kyle Xiao <kylexiao20@gmail.com>
2025-03-29 20:39:54 -0400

    home/graphics: indicate use wayland backend

--- home/session-variables.nix
+++ home/session-variables.nix
@@ -4,5 +4,8 @@
     VISUAL = "nvim";
     TERMINAL = "kitty";
     NIXPKGS_ALLOW_UNFREE = 1;
+    QT_QPA_PLATFORM = "wayland;xcb";
+    SDL_VIDEODRIVER = "wayland";
+    XDG_SESSION_TYPE = "wayland";
   };
 }
