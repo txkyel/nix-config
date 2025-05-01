bf1cb67f92504e20768c754dd69ee1b5258ef873
Kyle Xiao <kylexiao20@gmail.com>
2025-05-01 18:39:51 -0400

    hyprland: move cachix settings to hyprland.nix

--- system/hyprland.nix
+++ system/hyprland.nix
@@ -10,6 +10,10 @@ in
 {
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   programs.niri.enable = true;
+  nix.settings = {
+    substituters = [ "https://hyprland.cachix.org" ];
+    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+  };
   programs.hyprland = {
     enable = true;
     package = hyprland;
--- system/nixos-configuration.nix
+++ system/nixos-configuration.nix
@@ -11,9 +11,6 @@
       "nix-command"
       "flakes"
     ];
-    # Use cachix for hyprland
-    substituters = [ "https://hyprland.cachix.org" ];
-    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
   };
   nixpkgs.config.allowUnfree = true;
   nixpkgs.overlays = [
