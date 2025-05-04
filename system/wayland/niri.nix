0f7fdae24059367edadb017fafadae5a829c6611
Kyle Xiao <kylexiao20@gmail.com>
2025-05-03 23:23:49 -0400

    niri: flake package only

--- system/wayland/niri.nix
+++ system/wayland/niri.nix
@@ -1,6 +1,9 @@
 { inputs, pkgs, ... }:
 {
-  imports = [ inputs.niri.nixosModules.niri ];
+  nix.settings = {
+    substituters = [ "https://hyprland.cachix.org" ];
+    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+  };
   nixpkgs.overlays = [ inputs.niri.overlays.niri ];
 
   programs.niri.enable = true;
