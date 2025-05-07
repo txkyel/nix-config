be2fba9ed79d6f9950c43f332207a64c11477efa
Kyle Xiao <kylexiao20@gmail.com>
2025-05-07 17:42:50 -0400

    home/desktop-entries: hide unwanted desktop entries

--- home/desktop-entries.nix
+++ home/desktop-entries.nix
@@ -1,19 +1,41 @@
 {
-  xdg.desktopEntries = {
-    vesktop = {
-      name = "Vesktop";
-      exec = "vesktop %U --enable-blink-features=MiddleClickAutoscroll";
-      icon = "vesktop";
-      genericName = "Internet Messenger";
-      categories = [
-        "Network"
-        "InstantMessaging"
-        "Chat"
-      ];
-      settings = {
-        Keywords = "discord;vencord;electron;chat";
-        StartupWMClass = "Vesktop";
+  xdg.desktopEntries =
+    {
+      vesktop = {
+        name = "Vesktop";
+        exec = "vesktop %U --enable-blink-features=MiddleClickAutoscroll";
+        icon = "vesktop";
+        genericName = "Internet Messenger";
+        categories = [
+          "Network"
+          "InstantMessaging"
+          "Chat"
+        ];
+        settings = {
+          Keywords = "discord;vencord;electron;chat";
+          StartupWMClass = "Vesktop";
+        };
       };
-    };
-  };
+    }
+    // builtins.listToAttrs (
+      # Hide desktop entries
+      map
+        (name: {
+          name = name;
+          value = {
+            name = "";
+            noDisplay = true;
+          };
+        })
+        [
+          "kbd-layout-viewer5"
+          "rofi-theme-selector"
+          "xterm"
+          # TODO: Find thumbnailer replacement that handles audio/video thumbnailing
+          "org.gnome.Totem"
+          # fcitx5 desktop entries
+          "org.fcitx.fcitx5-migrator"
+          "kcm_fcitx5"
+        ]
+    );
 }
