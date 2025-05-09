c8af42855a281d9bf2a91e59215161c7277bc4fa
Kyle Xiao <kylexiao20@gmail.com>
2025-05-09 15:41:51 -0400

    ags: add ags extra packages, add systray

--- home/ags/ags-config/widget/Bar.tsx
+++ home/ags/ags-config/widget/Bar.tsx
@@ -1,31 +1,49 @@
-import { App, Astal, Gtk, Gdk } from "astal/gtk3"
-import { Variable } from "astal"
+import { App, Astal, Gtk, Gdk } from "astal/gtk3";
+import { Variable, bind } from "astal";
+import Tray from "gi://AstalTray";
 
-const time = Variable("").poll(1000, "date")
+const time = Variable("").poll(1000, "date");
+const tray = Tray.get_default();
+
+const SysTray = () => (
+  <box className="SysTray">
+    {bind(tray, "items").as((items) => {
+      return items.map((item) => {
+        return (
+          <menubutton
+            tooltipMarkup={bind(item, "tooltipMarkup")}
+            usePopover={false}
+            actionGroup={bind(item, "actionGroup").as((ag) => ["dbusmenu", ag])}
+            menuModel={bind(item, "menuModel")}
+          >
+            <icon gicon={bind(item, "gicon")} />
+          </menubutton>
+        );
+      });
+    })}
+  </box>
+);
 
 export default function Bar(gdkmonitor: Gdk.Monitor) {
-    const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
+  const { TOP, LEFT, RIGHT } = Astal.WindowAnchor;
 
-    return <window
-        className="Bar"
-        gdkmonitor={gdkmonitor}
-        exclusivity={Astal.Exclusivity.EXCLUSIVE}
-        anchor={TOP | LEFT | RIGHT}
-        application={App}>
-        <centerbox>
-            <button
-                onClicked="echo hello"
-                halign={Gtk.Align.CENTER}
-            >
-                Welcome to AGS!
-            </button>
-            <box />
-            <button
-                onClicked={() => print("hello")}
-                halign={Gtk.Align.CENTER}
-            >
-                <label label={time()} />
-            </button>
-        </centerbox>
+  return (
+    <window
+      className="Bar"
+      gdkmonitor={gdkmonitor}
+      exclusivity={Astal.Exclusivity.EXCLUSIVE}
+      anchor={TOP | LEFT | RIGHT}
+      application={App}
+    >
+      <centerbox>
+        <button onClicked="echo hello" halign={Gtk.Align.CENTER}>
+          Welcome to AGS!
+        </button>
+        <SysTray />
+        <button onClicked={() => print("hello")} halign={Gtk.Align.CENTER}>
+          <label label={time()} />
+        </button>
+      </centerbox>
     </window>
+  );
 }
--- home/ags/default.nix
+++ home/ags/default.nix
@@ -1,9 +1,11 @@
 {
   inputs,
+  pkgs,
   ...
 }:
 {
   imports = [ inputs.ags.homeManagerModules.default ];
 
   programs.ags.enable = true;
+  programs.ags.extraPackages = builtins.attrValues inputs.ags.packages.${pkgs.system};
 }
