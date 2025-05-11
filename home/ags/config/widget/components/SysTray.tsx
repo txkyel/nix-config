97870a96771603253df4b0329d2aca13cd16ee5b
Kyle Xiao <kylexiao20@gmail.com>
2025-05-11 12:23:00 -0400

    ags: copy Mabi19 tray implementation

--- home/ags/config/widget/components/SysTray.tsx
+++ home/ags/config/widget/components/SysTray.tsx
@@ -1,25 +1,71 @@
 import { bind } from "astal";
-import { hook } from "astal/gtk4";
+import { hook, Gdk, Gtk } from "astal/gtk4";
 import AstalTray from "gi://AstalTray";
 
 const tray = AstalTray.get_default();
 
+// https://github.com/Mabi19/desktop-shell/blob/b8f3c5ee1aa8c940a73474f5d82a8ee8996522d4/bar/tray.tsx
+// TODO: understand code and refactor
 const TrayItem = ({ item }: { item: AstalTray.TrayItem }) => {
-  return (
+  const popover = Gtk.PopoverMenu.new_from_model(item.menuModel);
+  const button = (
     <menubutton
+      popover={popover}
       cssClasses={["tray-item"]}
       tooltipMarkup={bind(item, "tooltipMarkup")}
       menuModel={bind(item, "menuModel")}
       setup={(self) => {
-        hook(self, item, "notify::action-group", () =>
-          self.insert_action_group("dbusmenu", item.action_group),
-        );
         self.insert_action_group("dbusmenu", item.action_group);
       }}
     >
       <image cssClasses={["tray-icon"]} gicon={bind(item, "gicon")} />
     </menubutton>
-  );
+  ) as Gtk.MenuButton;
+
+  const controller = new Gtk.EventControllerLegacy({
+    propagationPhase: Gtk.PropagationPhase.CAPTURE,
+  });
+  controller.connect("event", (_c, event) => {
+    const type = event.get_event_type();
+
+    if (type == Gdk.EventType.BUTTON_PRESS) {
+      const button = (event as Gdk.ButtonEvent).get_button();
+      if (
+        button == Gdk.BUTTON_SECONDARY ||
+        (item.is_menu && button == Gdk.BUTTON_PRIMARY)
+      ) {
+        // do this earlier for less flash-of-invalid-content
+        item.about_to_show();
+      }
+    }
+
+    if (type == Gdk.EventType.BUTTON_RELEASE) {
+      const pressEvent = event as Gdk.ButtonEvent;
+      const mouseButton = pressEvent.get_button();
+      const [_, x, y] = pressEvent.get_position();
+      if (pressEvent.get_surface() != button.get_native()?.get_surface()) {
+        // this also sometimes captures the click on the popup for some reason
+        return false;
+      }
+
+      if (mouseButton == Gdk.BUTTON_PRIMARY) {
+        if (item.is_menu) {
+          button.popup();
+        } else {
+          item.activate(x, y);
+        }
+      } else if (mouseButton == Gdk.BUTTON_MIDDLE) {
+        item.secondary_activate(x, y);
+      } else {
+        button.popup();
+      }
+    }
+    // Stop processing further.
+    return true;
+  });
+  button.add_controller(controller);
+
+  return button;
 };
 
 const SysTray = () => (
