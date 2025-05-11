72b64fe47594b5645d1da9a608ef4c676613d892
Kyle Xiao <kylexiao20@gmail.com>
2025-05-10 23:04:56 -0400

    ags: move tray item to separate component

--- home/ags/config/widget/components/SysTray.tsx
+++ home/ags/config/widget/components/SysTray.tsx
@@ -1,27 +1,31 @@
 import { bind } from "astal";
 import { hook } from "astal/gtk4";
-import Tray from "gi://AstalTray";
+import AstalTray from "gi://AstalTray";
 
-const tray = Tray.get_default();
+const tray = AstalTray.get_default();
+
+const TrayItem = ({ item }: { item: AstalTray.TrayItem }) => {
+  return (
+    <menubutton
+      cssClasses={["tray-item"]}
+      tooltipMarkup={bind(item, "tooltipMarkup")}
+      menuModel={bind(item, "menuModel")}
+      setup={(self) => {
+        hook(self, item, "notify::action-group", () =>
+          self.insert_action_group("dbusmenu", item.action_group),
+        );
+        self.insert_action_group("dbusmenu", item.action_group);
+      }}
+    >
+      <image cssClasses={["tray-icon"]} gicon={bind(item, "gicon")} />
+    </menubutton>
+  );
+};
 
 const SysTray = () => (
   <box cssClasses={["sys-tray"]}>
     {bind(tray, "items").as((items) =>
-      items.map((item) => (
-        <menubutton
-          cssClasses={["tray-item"]}
-          tooltipMarkup={bind(item, "tooltipMarkup")}
-          menuModel={bind(item, "menuModel")}
-          setup={(self) => {
-            hook(self, item, "notify::action-group", () =>
-              self.insert_action_group("dbusmenu", item.action_group),
-            );
-            self.insert_action_group("dbusmenu", item.action_group);
-          }}
-        >
-          <image cssClasses={["tray-icon"]} gicon={bind(item, "gicon")} />
-        </menubutton>
-      )),
+      items.map((item) => <TrayItem item={item} />),
     )}
   </box>
 );
