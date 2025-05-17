61d97b48db81043d2d940911d11616934396b56c
Kyle Xiao <kylexiao20@gmail.com>
2025-05-17 19:59:05 -0400

    ags: remove obs icon overrides for now

--- home/ags/config/utils/icon-overrides.ts
+++ home/ags/config/utils/icon-overrides.ts
@@ -1,11 +1,6 @@
 import { Gio } from "astal";
 
-const OVERRIDES = new Map<string, string>([
-  ["corectrl", "corectrl-tray"],
-  ["obs", "obs-tray"],
-  // TODO: figure out how to determine the icon state
-  ["???", "obs-active-tray"],
-]);
+const OVERRIDES = new Map<string, string>([["corectrl", "corectrl-tray"]]);
 
 export const get_icon_override = (key: string, fallback: Gio.Icon) => {
   if (OVERRIDES.has(key)) {
