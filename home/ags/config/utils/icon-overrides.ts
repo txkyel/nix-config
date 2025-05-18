013d59e842a5169d9e819f00510590e0fcea01b0
Kyle Xiao <kylexiao20@gmail.com>
2025-05-18 17:21:17 -0400

    ags: override to use default theme icon

--- home/ags/config/utils/icon-overrides.ts
+++ home/ags/config/utils/icon-overrides.ts
@@ -1,6 +1,6 @@
 import { Gio } from "astal";
 
-const OVERRIDES = new Map<string, string>([["corectrl", "corectrl-tray"]]);
+const OVERRIDES = new Map<string, string>([["corectrl", "corectrl-panel"]]);
 
 export const get_icon_override = (key: string, fallback: Gio.Icon) => {
   if (OVERRIDES.has(key)) {
