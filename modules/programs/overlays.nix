0e5098f66445eb01346f75e318736cf8bed0364d
Kyle Xiao <kylexiao20@gmail.com>
2025-06-09 09:48:27 -0400

    overlays: remove mpv fix

--- modules/programs/overlays.nix
+++ modules/programs/overlays.nix
@@ -14,27 +14,6 @@
             ''}
         '';
       });
-
-      # Holdover fix for mpv https://github.com/NixOS/nixpkgs/issues/412382#issuecomment-2924903019
-      # https://github.com/NixOS/nixpkgs/pull/412889
-      mpv-unwrapped = prev.mpv-unwrapped.override {
-        libplacebo = final.libplacebo-mpv;
-      };
-      libplacebo-mpv =
-        let
-          version = "7.349.0";
-        in
-        prev.libplacebo.overrideAttrs (old: {
-          inherit version;
-          src = prev.fetchFromGitLab {
-            domain = "code.videolan.org";
-            owner = "videolan";
-            repo = "libplacebo";
-            rev = "v${version}";
-            hash = "sha256-mIjQvc7SRjE1Orb2BkHK+K1TcRQvzj2oUOCUT4DzIuA=";
-          };
-        });
     })
-
   ];
 }
