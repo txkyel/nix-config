80e70ea15f837b7e4aa61de95d3db4e6f894c0cf
Kyle Xiao <kylexiao20@gmail.com>
2025-06-26 08:31:51 -0400

    nixos-configuration: migrate to core modules

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     ./desktop
+    ./nixos-configuration.nix
     ./users.nix
     ./session-variables.nix
   ];
--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -3,7 +3,6 @@
     ./bootloader.nix
     ./localization.nix
     ./network.nix
-    ./nixos-configuration.nix
     ./security.nix
     ./services.nix
     ./sound.nix
