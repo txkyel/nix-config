2fae33ee4481661a9114d71be184896ae2689189
Kyle Xiao <kylexiao20@gmail.com>
2025-08-09 20:04:25 -0400

    homelab/qbittorrent: use module added to stable

--- modules/homelab/services/qbittorrent.nix
+++ modules/homelab/services/qbittorrent.nix
@@ -1,22 +1,7 @@
-# NOTE copies bits and pieces from https://github.com/NixOS/nixpkgs/pull/287923
-# Migrate to using this service config once it's been merged
-{
-  config,
-  lib,
-  pkgs,
-  utils,
-  ...
-}:
+{ config, lib, ... }:
 let
-  inherit (builtins) toString;
-  inherit (lib)
-    mkEnableOption
-    mkOption
-    mkPackageOption
-    mkIf
-    getExe
-    ;
-  inherit (lib.types) path port;
+  inherit (lib) mkEnableOption mkIf;
+
   service = "qbittorrent";
   homelab = config.homelab;
   cfg = homelab.services.${service};
@@ -24,54 +9,17 @@ in
 {
   options.homelab.services.${service} = {
     enable = mkEnableOption "Enable ${service}";
-
-    package = mkPackageOption pkgs "qbittorrent-nox" { };
-
-    profileDir = mkOption {
-      type = path;
-      default = "/var/lib/qBittorrent";
-      description = "The path passed to qBittorrent via --profile.";
-    };
-
-    port = mkOption {
-      type = port;
-      default = 8080;
-      description = "qBittorrent WebUI port.";
-    };
   };
 
   config = mkIf (homelab.enable && cfg.enable) {
-    networking.firewall.allowedTCPPorts = [ cfg.port ];
-
-    systemd = {
-      tmpfiles.rules = [
-        "d /var/lib/qBittorrent ${homelab.user} ${homelab.group} - -"
-      ];
-
-      services.qbittorrent = {
-        description = "qBittorrent-nox service";
-        documentation = [ "man:qbittorrent-nox(1)" ];
-        wants = [ "network-online.target" ];
-        after = [
-          "local-fs.target"
-          "network-online.target"
-          "nss-lookup.target"
-        ];
-        wantedBy = [ "multi-user.target" ];
-
-        serviceConfig = {
-          Type = "simple";
-          User = homelab.user;
-          Group = homelab.group;
-
-          ExecStart = utils.escapeSystemdExecArgs [
-            (getExe cfg.package)
-            "--profile=${cfg.profileDir}"
-            "--webui-port=${toString cfg.port}"
-          ];
-          TimeoutStopSec = 1800;
-          PrivateTmp = false;
-        };
+    # Note: You'll have to open up service logs to find the password when first
+    # accessing the WebUI
+    services.qbittorrent = {
+      enable = true;
+      user = homelab.user;
+      group = homelab.user;
+      serverConfig = {
+        LegalNotice.Accepted = true;
       };
     };
   };
