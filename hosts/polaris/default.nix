de5d652e1e87e9cc708ea95e3e85cf0a65b71037
Kyle Xiao <kylexiao20@gmail.com>
2026-08-23 18:54:30 -0400

    flake: update

--- hosts/polaris/default.nix
+++ hosts/polaris/default.nix
@@ -5,7 +5,7 @@
 {
   imports = [
     inputs.nixos-hardware.nixosModules.gigabyte-b550
-    inputs.nixos-hardware.nixosModules.common-cpu-amd-zenpower
+    inputs.nixos-hardware.nixosModules.common-cpu-amd
     inputs.nixos-hardware.nixosModules.common-gpu-amd
     ./hardware-configuration.nix
     ./options.nix
